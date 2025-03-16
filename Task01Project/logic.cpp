11e981781594575914f85237f6affdcf4070655a
ivanvikvik <ivanvikvik@gmail.com>
2025-03-16 18:51:04 +0300

    Add project files.

---
+++ Task01Project/logic.cpp
@@ -0,0 +1,22 @@
+#include "logic.h"
+#include <iostream>
+using namespace std;
+
+// O(N^2)
+void bubble_sort(int* array, int size, bool type) {
+	int count = 0;
+	for (int j = 0; j < size - 1; j++)
+	{
+		for (int i = 0; i < size - 1; i++)
+		{
+			count++;
+			if (type ? array[i] > array[i + 1] : array[i] < array[i + 1]) {
+				int t = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = t;
+			}
+		}
+	}
+	
+	cout << "Count: " << count << endl;
+}
