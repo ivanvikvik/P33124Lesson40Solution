0680b418e2cb04f9498772a3532fa3af10566ed0
ivanvikvik <ivanvikvik@gmail.com>
2025-03-16 19:42:36 +0300

    Update project

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -1,22 +1,25 @@
 #include "logic.h"
-#include <iostream>
-using namespace std;
 
 // O(N^2)
+// best-case: O(N)
 void bubble_sort(int* array, int size, bool type) {
-	int count = 0;
 	for (int j = 0; j < size - 1; j++)
 	{
-		for (int i = 0; i < size - 1; i++)
+		bool flag = true;
+		
+		for (int i = 0; i < size - 1 - j; i++)
 		{
 			count++;
 			if (type ? array[i] > array[i + 1] : array[i] < array[i + 1]) {
+				flag = false;
 				int t = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = t;
 			}
 		}
+		
+		if (flag) {
+			break;
+		}
 	}
-	
-	cout << "Count: " << count << endl;
 }
--- Task01Project/main.cpp
+++ Task01Project/main.cpp
@@ -1,18 +1,19 @@
 #include "util.h"
 #include "logic.h"
-#define SIZE 10
+#define SIZE 1000000
 
 int main() {
-	int array[SIZE]{ 9,8,7,6,5,4,3,2,1,0 };
+	int array[SIZE];// { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 
-	//init(array, SIZE, -50, 50);
+	init(array, SIZE, -50, 50);
 	cout << "Array before sorting: " << convert(array, SIZE) << endl;
+		
+	//bubble_sort(array, SIZE, false);
+	cout << "Array after sorting: " << convert(array, SIZE) << endl;
 
 	bubble_sort(array, SIZE, true);
 	cout << "Array after sorting: " << convert(array, SIZE) << endl;
 
-	bubble_sort(array, SIZE, false);
-	cout << "Array after sorting: " << convert(array, SIZE) << endl;
 
 	return 0;
 }
